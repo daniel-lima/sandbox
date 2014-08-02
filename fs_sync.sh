20ceed888474397654a5b224d2d455c76adf3968
Daniel Henrique Alves Lima <email_daniel_h@yahoo.com.br>
2014-08-02 15:33:57 -0300

    Watches and rsyncs a directory structure

--- fs_sync.sh
+++ fs_sync.sh
@@ -22,27 +22,57 @@ function install_rsync() {
     fi
 }
 
+function install_sshpass() {
+    if ! which sshpass; then
+	if which sshpass; then
+	    echo 'Installing sshpass'
+	    sudo apt-get -y install sshpass
+	fi
+    fi
+}
+
 
 function run() {
+    if [ $# -ne 2 ]; then
+	echo "Usage: $0 local_path remote_user@remote_host:remote_path"
+	exit 1
+    fi
+
     local _local_path=$1
     local _remote_destiny=$2
+    local _rsync_extra_arg=
 
+
+    if [ -n "${SSHPASS}" ]; then
+	echo "It will use SSHPASS environment variable"
+	install_sshpass
+
+	local _remote_user=`echo "${_remote_destiny}" | cut -f1 -d'@'`
+	_remote_destiny=`echo "${_remote_destiny}" | cut -f2 -d'@'`
+
+	_rsync_extra_arg="--rsh='sshpass -e ssh -l ${_remote_user}'"
+    fi
+
+
+    # http://stackoverflow.com/questions/20370566/inotify-and-rsync-on-large-number-of-files
     local _events="CREATE,DELETE,MODIFY,MOVED_FROM,MOVED_TO"
     
     echo "inotifywait -e '${_events}' -m -r --format '%:e %f' '${_local_path}' --excludei '\.git' --excludei '\.svn' --excludei '.\+~'"
     inotifywait -e "${_events}" -m -r --format '%:e %f' "${_local_path}" --excludei '\.git' --excludei '\.svn' --excludei '.\+~' | (
-	local _waiting="";
+	local _reading_events="";
 	while true; do
-	    local _line="";
+	    local _line=;
 	    read -t 1 _line;
 	    if test -z "${_line}"; then
-		if test ! -z "${_waiting}"; then
+		if test -n "${_reading_events}"; then
                     echo "CHANGE";
-                    _waiting="";
-                    #rsync --update --archive --links --safe-links --verbose --compress --recursive --exclude '\.git' --exclude '\.svn' --exclude '.\+~' "${_local_path}/*" "${_remote_destiny}"
+                    _reading_events=;
+		    echo "rsync --update --archive --links --safe-links --verbose --recursive --exclude '\.git' --exclude '\.svn' --exclude '.\+~' '${_rsync_extra_arg}' '${_local_path}/*' '${_remote_destiny}'"
+                    #rsync --update --archive --links --safe-links --verbose --recursive --exclude '\.git' --exclude '\.svn' --exclude '.\+~' "${_rsync_extra_arg}" "${_local_path}/*" "${_remote_destiny}"
 		fi;
 	    else
-		_waiting=1;
+		# It will read all events before trying to rsync anything
+		_reading_events=1;
 	    fi;
 	    done)
     
